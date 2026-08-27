cbef88a70632925b66c12a4a970f9524b536aa93
eloah7373 <eloahborges.eb1@gmail.com>
2026-08-27 13:48:18 +0000

    é isso aíiiii

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect;
 
 app = Flask(__name__)
 
 @app.route("/")
-def ola_mundo():
- return render_template("index.html")
+def login():
+ return render_template("login.html")
+
+@app.route("/painel")
+def painel():
+ return render_template("painel.html")
+
+@app.route("/verificar", methods=['POST'])
+def verificar():
+    CPF = request.form.get('CPF')
+    SENHA = request.form.get('SENHA')
+
+    print('Tentando login com:', CPF, '/SENHA:',SENHA)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
